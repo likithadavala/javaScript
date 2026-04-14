eb08a3ea30ee87942aff8a29d6cb4e9fbc0ce37f
likithadavala <likithadavala99@gmail.com>
2026-04-14 13:49:09 +0530

    javaScriptArray-concepts

--- JsFiles/DotReplace.js
+++ JsFiles/DotReplace.js
@@ -1,7 +1,1 @@
-function onClickDotReplace(){
-    debugger;
-    var sentences = document.getElementById("txtSentences").value;
-    var word = document.getElementById("txtWord").value;
-    var replaceWord = document.getElementById("txtReplaceWord").value;
-    document.getElementById("pResult").innerHTML=sentences.replaceAll(word,replaceWord);
-}
+huuigyyuy
