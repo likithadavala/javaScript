e6096b0074b7a397839ae194fe91bdd3f59b2eea
Likithadavala <likithadavala99@gmail.com>
2026-05-19 15:45:39 +0530

    FunctionsOfNumber and ArrowFunctions

--- ArrayConcepts/Js.file/FunctionsOfNumber.js
+++ ArrayConcepts/Js.file/FunctionsOfNumber.js
@@ -1,8 +1,15 @@
+//function is inside parameters or methods is called functions
+//By passing or declared is called function
+//invoke means calling or executed a function is invoke
+//The value passed during function invction is called an argument
+//return or executed the function is called return value
+//we can link the jsfile 
+//
 function onClickAddition(){
     debugger;
     var firstNumber= Number(document.getElementById("txtFirstNumber").value);
     var secondNumber =Number(document.getElementById("txtSecondNumber").value);    
-    document.getElementById("pAddResult").innerHTML=calucator(firstNumber,secondNumber,'add');
+    document.getElementById("pAddResult").innerHTML=calucator(firstNumber,secondNumber,'add');//invoking calculator methods by passing firstnumber,secondnumber,add as arguments
             
 }
 
@@ -10,32 +17,51 @@ function onClickSubtraction(){
     debugger;
     var firstNumber=Number(document.getElementById("txtFirstNumber").value);
     var secondNumber =Number(document.getElementById("txtSecondNumber").value);    
-    document.getElementById("pSubResult").innerHTML=calucator(firstNumber,secondNumber,'sub');
-            
+    document.getElementById("pSubResult").innerHTML=calucator(firstNumber,secondNumber,'sub');//invoking calculator methods by passing firstnumber,secondnumber,sub as arguments                    
 }
 function onClickMultiplication(){
     debugger;
     var firstNumber= Number(document.getElementById("txtFirstNumber").value);
     var secondNumber =Number(document.getElementById("txtSecondNumber").value);    
-    document.getElementById("pMultipleResult").innerHTML=calucator(firstNumber,secondNumber,'multiple');
-}0
+    document.getElementById("pMultipleResult").innerHTML=calucator(firstNumber,secondNumber,'multiple');//invoking calculator methods by passing firstnumber,secondnumber,multiple as arguments
+}
 function onClickDivision(){
     debugger;
     var firstNumber= Number(document.getElementById("txtFirstNumber").value);
     var secondNumber =Number(document.getElementById("txtSecondNumber").value);    
-    document.getElementById("pDivisionResult").innerHTML=calucator(firstNumber,secondNumber,'division');
+    document.getElementById("pDivisionResult").innerHTML=calucator(firstNumber,secondNumber,'division');//invoking calculator methods by passing firstnumber,secondnumber,division as arguments
 }
 function onClickRemainder(){
     debugger;
     var firstNumber= Number(document.getElementById("txtFirstNumber").value);
     var secondNumber =Number(document.getElementById("txtSecondNumber").value);    
-    document.getElementById("pRemainderResult").innerHTML=calucator(firstNumber,secondNumber,'Remainder');
+    document.getElementById("pRemainderResult").innerHTML=calucator(firstNumber,secondNumber,'Remainder');//invoking calculator methods by passing firstnumber,secondnumber,remainder as arguments
 }
 
 
 
-function calucator(firstNumber,secondNumber,operator){
-debugger;
+// function calucator(firstNumber,secondNumber,operator){//function declaration with name calculator,firstnumber,secondnumber,add is parameters
+// debugger;
+
+//     if(operator=='add'){
+//         return firstNumber+secondNumber;
+//     }
+//        if(operator=='sub'){
+//         return firstNumber-secondNumber;
+//     }
+//        if(operator=='multiple'){
+//          return firstNumber*secondNumber;
+//     }  
+//     if(operator=='division'){
+//         return firstNumber/secondNumber;
+//     } 
+//     if(operator=='Remainder'){
+//         return firstNumber%secondNumber;
+//     }
+ 
+// }
+const calucator=(firstNumber,secondNumber,operator)=>{
+    debugger;
     if(operator=='add'){
         return firstNumber+secondNumber;
     }
@@ -51,5 +77,5 @@ debugger;
     if(operator=='Remainder'){
         return firstNumber%secondNumber;
     }
- 
+    
 }
