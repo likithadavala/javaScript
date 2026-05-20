921f7795fa883eb809a6ecb66be9c1cdf0784f65
Likithadavala <likithadavala99@gmail.com>
2026-05-20 11:08:32 +0530

    Arrow Functions(arrow equalsto)

---
+++ ArrayConcepts/Js.file/ArrowEqualsto.js
@@ -0,0 +1,43 @@
+
+function onClickSquare(){
+    debugger;
+    var number=Number(document.getElementById("txtNumber").value);
+    document.getElementById("pSquareResult").innerHTML=square(number);
+    document.getElementById("txtNumber").value="";
+}
+function onClickCube(){
+    debugger;
+    var number=Number(document.getElementById("txtNumber").value);
+    document.getElementById("pCubeResult").innerHTML=cube(number);
+    document.getElementById("txtNumber").value="";
+}
+function onClickMutipleOfNumber(){
+    debugger;
+    var number=Number(document.getElementById("txtNumber").value);
+    document.getElementById("pMultipleOfNumberResult").innerHTML=MultipleOfNumber(number);
+    document.getElementById("txtNumber").value="";
+}
+function onClickEvenOrOdd(){
+    debugger;
+    var number=Number(document.getElementById("txtNumber").value);
+    document.getElementById("pEvenOrOddResult").innerHTML=EvenOrOdd(number);
+    document.getElementById("txtNumber").value="";
+}
+
+
+const square=(number)=>{
+    return number*number;
+}
+const cube=(number)=>{
+    return number*number;
+}
+const MultipleOfNumber=(number)=>{
+    return number*number;
+}
+const EvenOrOdd=(number)=>{
+    if (number % 2 == 0) {
+        return "it is an even number";
+    }else {
+        return "It is an odd number";
+    }
+}
